d2f6d57c2049e32b5180db91436d77b1c902a899
cobrce <cob_rce@hotmail.com>
2020-09-25 01:10:21 +0100

    Fixed ESPTimerHelper.h by including esp32-hal.h

--- ESPTimerHelper.h
+++ ESPTimerHelper.h
@@ -1,6 +1,7 @@
 #ifndef ESPTIMERHELPER_H_
 #define ESPTIMERHELPER_H_
 
+#include <esp32-hal.h>
 #define DIVIDER 80
 void StartTimer(hw_timer_t * timer);
 hw_timer_t * SetTimer(int timer,void(*callback)(),int periode_us = 10);
